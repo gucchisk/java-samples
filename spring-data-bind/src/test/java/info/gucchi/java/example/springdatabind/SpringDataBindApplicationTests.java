038d71451af0833de8ec38a119a8494cbcbafbc0
gucchisk <gucchi_sk@yahoo.co.jp>
2020-05-10 13:44:48 +0900

    tweak

---
+++ spring-data-bind/src/test/java/info/gucchi/java/example/springdatabind/SpringDataBindApplicationTests.java
@@ -0,0 +1,13 @@
+package info.gucchi.java.example.springdatabind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataBindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
