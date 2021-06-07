a4d9864d75614405f275a4cdfd9e9cebbc9fccad
gucchisk <gucchi_sk@yahoo.co.jp>
2021-06-08 00:46:37 +0900

    Add properties

--- properties/src/main/java/info/gucchi/sample/Properties.java
+++ properties/src/main/java/info/gucchi/sample/Properties.java
@@ -4,10 +4,10 @@ import java.util.Map;
 
 public class Properties {
     public static void main(String[] args) {
-	System.out.println("jdk.version: " + System.getProperty("jdk.version"));
-	System.out.println("java.version: " + System.getProperty("java.version"));
-        var text = "hello";
+        System.out.println("jdk.version: " + System.getProperty("jdk.version"));
+        System.out.println("java.version: " + System.getProperty("java.version"));
+        String text = "hello";
         System.out.println(text);
-	System.out.println(System.getProperty("text"));
+        System.out.println(System.getProperty("text"));
     }
 }
