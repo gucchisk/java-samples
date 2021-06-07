28547e639f002b776aa6cf943be5f7c2154e2a85
gucchisk <gucchi_sk@yahoo.co.jp>
2021-06-08 00:55:36 +0900

    Remove upload

--- upload/src/main/java/info/gucchi/sample/api/upload/FileStorageException.java
+++
@@ -1,7 +0,0 @@
-package info.gucchi.sample.api.upload;
-
-public class FileStorageException extends RuntimeException {
-    public FileStorageException(String message) {
-        super(message);
-    }
-}
