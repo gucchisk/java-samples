d711357a2a1f31298e1bc58b3c5d97932a336142
gucchisk <gucchi_sk@yahoo.co.jp>
2023-02-03 05:30:56 +0900

    Refactor

--- spring-boot-i18n/src/main/java/info/gucchi/sample/springbooti18n/IndexController.java
+++ spring-boot-i18n/src/main/java/info/gucchi/sample/springbooti18n/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Calendar;
+
 @Controller
 public class IndexController {
     @Autowired
@@ -14,8 +16,21 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        String message = messageSource.getMessage("message", null, LocaleContextHolder.getLocale());
+        Calendar calendar = Calendar.getInstance();
+        String messageKey = getMessageKey(calendar);
+        String message = messageSource.getMessage(messageKey, null, LocaleContextHolder.getLocale());
         model.addAttribute("message", message);
         return "index";
+    }
+
+    private String getMessageKey(Calendar calendar) {
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (hour > 3 && hour < 11) {
+            return "message_morning";
+        } else if (hour >= 11 && hour < 19) {
+            return "message_noon";
+        } else {
+            return "message_night";
+        }
     }
 }
